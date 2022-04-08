6f3faaa37ca8953d049c0d2aaedf247b2c34e274
Maxim Ermilov <max-ermilov@yandex.ru>
2022-04-08 18:12:20 +0300

    add profile-info on load

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
@@ -6,9 +6,9 @@ import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
 
 function App() {
-  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
-  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
-  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
 
   const handleEditProfileClick = () => {
     setIsEditProfilePopupOpen(true)
@@ -42,14 +42,37 @@ function App() {
                      isOpen={isEditProfilePopupOpen}
                      onClose={closeAllPopups}
       >
-            <input type="text" className="popup__input popup__input_field_name" name="name" id="name" placeholder="Имя"
-                   aria-label="Имя" minLength={2} maxLength={40} required/>
-            <span className="popup__input-error" id="name-error"/>
-            <input type="text" className="popup__input popup__input_field_job" name="job" id="job"
-                   placeholder="Род занатий" aria-label="Род занатий" minLength={2} maxLength={200} required/>
-            <span className="popup__input-error" id="job-error"/>
-            <input className="button popup__submit-btn popup__save-input-btn popup__submit-btn_inactive" type="submit"
-                   defaultValue="Cохранить" name="save"/>
+            <input type="text"
+                   className="popup__input popup__input_field_name"
+                   name="name"
+                   id="name"
+                   placeholder="Имя"
+                   aria-label="Имя"
+                   minLength={2}
+                   maxLength={40}
+                   required
+            />
+            <span className="popup__input-error"
+                  id="name-error"
+            />
+            <input type="text"
+                   className="popup__input popup__input_field_job"
+                   name="job"
+                   id="job"
+                   placeholder="Род занатий"
+                   aria-label="Род занатий"
+                   minLength={2}
+                   maxLength={200}
+                   required
+            />
+            <span className="popup__input-error"
+                  id="job-error"
+            />
+            <input className="button popup__submit-btn popup__save-input-btn popup__submit-btn_inactive"
+                   type="submit"
+                   defaultValue="Cохранить"
+                   name="save"
+            />
       </PopupWithForm>
 
       <PopupWithForm name="add-card"
@@ -57,14 +80,34 @@ function App() {
                      isOpen={isAddPlacePopupOpen}
                      onClose={closeAllPopups}
       >
-            <input type="text" className="popup__input popup__input_field_title" name="title" id="title"
-                   placeholder="Название" aria-label="Название" minLength={2} maxLength={30} required/>
-            <span className="popup__input-error" id="title-error"/>
-            <input type="url" className="popup__input popup__input_field_link" name="link" id="link"
-                   placeholder="Ссылка на картинку" aria-label="Название" required/>
-            <span className="popup__input-error" id="link-error"/>
-            <input className="button popup__submit-btn popup__create-btn" type="submit" defaultValue="Создать"
-                   name="create"/>
+            <input type="text"
+                   className="popup__input popup__input_field_title"
+                   name="title" id="title"
+                   placeholder="Название"
+                   aria-label="Название"
+                   minLength={2}
+                   maxLength={30}
+                   required
+            />
+            <span className="popup__input-error"
+                  id="title-error"
+            />
+            <input type="url"
+                   className="popup__input popup__input_field_link"
+                   name="link"
+                   id="link"
+                   placeholder="Ссылка на картинку"
+                   aria-label="Название"
+                   required
+            />
+            <span className="popup__input-error"
+                  id="link-error"
+            />
+            <input className="button popup__submit-btn popup__create-btn"
+                   type="submit"
+                   defaultValue="Создать"
+                   name="create"
+            />
       </PopupWithForm>
 
       <ImagePopup />
@@ -72,7 +115,11 @@ function App() {
       <PopupWithForm name="delete-confirm"
                      title="Вы уверены?"
       >
-            <button className="button popup__submit-btn popup__confirm-btn" type="submit" value="Да" name="confirm">Да
+            <button className="button popup__submit-btn popup__confirm-btn"
+                    type="submit"
+                    value="Да"
+                    name="confirm">
+              Да
             </button>
       </PopupWithForm>
 
@@ -81,11 +128,21 @@ function App() {
                      isOpen={isEditAvatarPopupOpen}
                      onClose={closeAllPopups}
       >
-            <input type="url" className="popup__input popup__input_field_avatar" name="avatar" id="avatar"
-                   placeholder="Ссылка на картинку" aria-label="Аватар" required/>
-            <span className="popup__input-error" id="avatar-error"/>
-            <input className="button popup__submit-btn popup__save-btn" type="submit" defaultValue="Сохранить"
-                   name="save"/>
+            <input type="url"
+                   className="popup__input popup__input_field_avatar"
+                   name="avatar" id="avatar"
+                   placeholder="Ссылка на картинку"
+                   aria-label="Аватар"
+                   required
+            />
+            <span className="popup__input-error"
+                  id="avatar-error"
+            />
+            <input className="button popup__submit-btn popup__save-btn"
+                   type="submit"
+                   defaultValue="Сохранить"
+                   name="save"
+            />
       </PopupWithForm>
 
       <template className="card-template">
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { api } from "../utils/Api";
 
 function Main(props) {
+  const [userName, setUserName] = useState('');
+  const [userDescription, setUserDescription] = useState('');
+  const [userAvatar, setUserAvatar] = useState('');
+
+  useEffect(() => {
+    api.getProfile()
+      .then(({name, about, avatar}) => {
+        setUserName(name)
+        setUserDescription(about)
+        setUserAvatar(avatar)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [])
+
   return(
     <main className="content page__content">
       <section className="profile">
         <div className="profile__content">
           <div className="profile__avatar-container">
-            <img alt="Фото профиля" className="profile__avatar" />
+            <img alt="Фото профиля"
+                 className="profile__avatar"
+                 src={userAvatar}
+            />
             <button className="button profile__avatar-edit-btn"
                     aria-label="Редактировать аватар"
                     type="button"
@@ -15,14 +35,14 @@ function Main(props) {
           </div>
           <div className="profile__info">
             <div className="profile__name">
-              <h1 className="profile__name-text" />
+              <h1 className="profile__name-text">{userName}</h1>
               <button className="button profile__edit-btn"
                       aria-label="Редактировать профиль"
                       type="button"
                       onClick={props.onEditProfile}
               />
             </div>
-            <p className="profile__job" />
+            <p className="profile__job">{userDescription}</p>
           </div>
         </div>
         <button className="button profile__add-place-btn"
